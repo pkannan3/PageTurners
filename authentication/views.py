45ea437b0f76313fe6deffa129216ce436385576
Priyanka Kannan <priyankannan91@gmail.com>
2024-06-06 16:43:51 -0500

    Registration - view.py (need to add ReactJS path)

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
 
-# Create your views here.
+
+def sign_up_view(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            confirm_password = form.cleaned_data["confirm_password"]
+            if password == confirm_password:
+                user = User.objects.create_user(
+                    username=username,
+                    password=password,
+                )
+                login(request, user)
+                return redirect("dashboard")
+            else:
+                form.add_error("password", "Password did not match")
+        else:
+            form = SignUpForm()
+        context = {
+            "forrm": form,
+        }
+        # enter reactjs file here
+        return render(request, "authentication/...", context)
